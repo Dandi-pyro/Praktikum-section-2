602f81ad438a0e8f338f2ea15c76df4e28161435
Dandi M. Reza <dandimr16@gmail.com>
2022-02-22 16:44:00 +0700

    Playlist Spotify

--- dart_section2_playlist_spotify/bin/dart_section2_playlist_spotify.dart
+++ dart_section2_playlist_spotify/bin/dart_section2_playlist_spotify.dart
@@ -1,3 +1,11 @@
 void main(List<String> arguments) {
   List<String> musik = [];
 }
+
+void DeleteSongOnPlaylistSpotify(List musik, String namaMusik){
+  musik.remove(namaMusik);
+}
+
+void ShowList(List musik){
+  print (musik);
+}
